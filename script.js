6b4260312289bc5edf6d403b8af6a005bcda98bf
manoeljoao673-jpg <manoeljoao673@gmail.com>
2026-07-04 10:46:34 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const sections = document.querySelectorAll("section");
+
+window.addEventListener("scroll", () => {
+
+    sections.forEach(section => {
+
+        const top = section.getBoundingClientRect().top;
+
+        if(top < window.innerHeight - 100){
+            section.classList.add("show");
+        }
+
+    });
+
+});
